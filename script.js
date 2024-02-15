b74686d012bac2a38d19badf6149736b1b0b66af
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-02-15 15:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let sum=0,ans=0;
+ for(let i=0;i<arr.length;i++){
+	 sum += arr[i];
+		 ans += sum;
+ }
+	return ans;
 }
 
 module.exports=mincost;
