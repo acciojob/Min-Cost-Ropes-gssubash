eb9e1d46d791984cf974feed731df3754c33c603
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-02-16 22:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function mincost(arr)
   let sum = 0,
     ans = 0;
   if (arr.length < 2) return arr[0];
-  console.log(arr);
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
     if (i > 0) { 
