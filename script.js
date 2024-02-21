a3e9b7243b448a27cb6d8e0c24b32beca569a56f
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-02-21 07:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-	arr.sort((a, b) => a - b);
   let sum = 0,
     ans = 0;
-  if (arr.length < 2) return arr[0];
+	let arrLen = arr.length;
+  if (arrLen < 2) return arr[0];
+while(i<arrLen){
+	arr.sort((a, b) => a - b);
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
     if (i > 0) { 
-
       ans += sum;
-    }
-    
-  }
+		arr.splice(0,2,sum);
+		break;
+	    }
+	  }
+	}
   return ans;
 }
 
